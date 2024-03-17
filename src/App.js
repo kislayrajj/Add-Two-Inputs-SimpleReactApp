3b1b9bdadfdadc52b1b3ba7966e813c216daec4f
Kislay Raj <kislayraj751@gmail.com>
2024-03-17 23:51:03 +0530

    first and last commit on add two input apps

--- src/App.js
+++ src/App.js
@@ -3,56 +3,56 @@ import React from "react";
 import { useState } from "react";
 
 const App = () => {
-  const [userInput, setUserInput] = useState("");
-  const [displayValue, setDisplayValue] = useState("");
+const [firstInput,setFirstInput] = useState("")
+const [secondInput,setSecondInput] = useState("");
+const [displayValue,setDisplayValue] = useState("");
 
-  const handleEnter = (e) => {
-    setUserInput(e.target.value);
-  };
+const handleEnter=(e)=>{
+  setFirstInput(e.target.value)
+  setDisplayValue("")
+}
 
-  const handleClick = () => {
-    if (userInput !== "") {
-      let newValue =
-        userInput[0].toUpperCase() + userInput.slice(1).toLocaleLowerCase();
-      let finalValue = newValue.slice(0, 20);
-      setDisplayValue(finalValue);
-      setUserInput("");
-    } else {
-      let promptInput = prompt("Plese Enter Your Name");
-      setDisplayValue(promptInput);
-      setUserInput("");
-    }
-  };
+const handleEnter2=(e)=>{
+  setSecondInput(e.target.value)
+  setDisplayValue("")
+}
+const AddInputs=()=>{
+  if(firstInput!== "" && secondInput!== ""){
+  let result = parseInt(firstInput)+parseInt(secondInput);
+  let output = `${firstInput} + ${secondInput} = ${result}`;
+  setDisplayValue(output);
+  setFirstInput("");
+  setSecondInput("");
+  }else if(firstInput== "" || secondInput== ""){
+    alert("Please enter two numbers");  
+  }
+}
 
-  const handleReset = () => {
-    setDisplayValue("");
-  };
+const handleReset=()=>{
+  setFirstInput("")
+  setSecondInput("")
+  setDisplayValue("")
+}
+
+
+
 
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className=" m-2 p-5 flex flex-col h-1/2 min-w-1/2  border-2 border-black justify-center gap-8 items-center bg-yellow-200">
-        <input
-          type="text"
-          name="Your Name"
-          id="name"
-          placeholder="Enter Your Text"
-          value={userInput}
-          onChange={handleEnter}
-          className="border-2 border-black  h-12 "
-        />
-        <button
-          onClick={handleClick}
-          className="p-2 h-12 w-24 border-2 border-black rounded-xl bg-green-200 hover:bg-green-400">
-          Show{" "}
-        </button>
-        <div className=" h-12 w-full p-2 border-2 border-black bg-white ">
-          {displayValue}
+    <div className="h-screen flex justify-center items-center  ">
+      <div className="h-1/2 min-w-1/2 p-2 border-2 border-black rounded-3xl bg-red-200/20 ">
+        <h1 className="p-2 text-center text-xl font-semibold tracking-wider">Enter Values to Add</h1>
+        <div className=" mt-10 flex justify-center gap-12 ">
+          <input type="number" placeholder="enter a number" onChange={handleEnter} value={firstInput} className="p-1 w-1/3 h-12 border-2 border-black rounded-md"/>
+          <input type="number" placeholder="enter a number" onChange={handleEnter2} value={secondInput} className="p-1  w-1/3 h-12  border-2 border-black rounded-md"/>
         </div>
-        <button
-          onClick={handleReset}
-          className="p-2 h-12 w-24 border-2 border-black rounded-xl bg-red-300 hover:bg-red-400">
-          Reset
-        </button>
+        <div className=" m-2 pt-10 flex justify-center gap-12">
+        <button onClick={AddInputs} className="p-2 w-1/3 h-12 border-2 border-black rounded-md bg-green-200 hover:bg-green-400">Add</button>
+        <div className=" p-2 min-w-52 h-24 border-2 border-black rounded-md bg-white">{displayValue}</div>
+        </div>
+       <div className="flex justify-center p-2 mt-4">
+       <button onClick={handleReset} className="p-2 w-1/3 h-12 border-2 border-black rounded-md text-white bg-black/60 hover:bg-black/90">Reset</button>
+       </div>
+      
       </div>
     </div>
   );
