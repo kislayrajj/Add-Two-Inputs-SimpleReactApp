e5c48b1e62a80d79b9a2ee96444da98bdad42075
Kislay Raj <kislayraj751@gmail.com>
2024-03-17 22:40:53 +0530

    first and last commit for simple userInput app

--- src/App.js
+++ src/App.js
@@ -1,54 +1,59 @@
 import "./App.css";
 import React from "react";
+import Counter from "./Apps/Counter.js";
 import { useState } from "react";
-const App = () => {
-  const [count, setCount] = useState(0);
 
-  const increaseCount = () => {
-    setCount(count + 1);
-    document.querySelector(".count-box ").style.color = "green";
+const App = () => {
+  const [userInput, setUserInput] = useState("");
+  const [displayValue, setDisplayValue] = useState("");
+
+  const handleEnter = (e) => {
+    setUserInput(e.target.value);
   };
 
-  const decreaseCount = () => {
-    if (count > -5) {
-      setCount(count - 1);
-      document.querySelector(".count-box ").style.color = "red";
+  const handleClick = () => {
+    if (userInput !== "") {
+      let newValue =
+        userInput[0].toUpperCase() + userInput.slice(1).toLocaleLowerCase();
+      let finalValue = newValue.slice(0, 20);
+      setDisplayValue(finalValue);
+      setUserInput("");
     } else {
-      setCount(0);
-      alert("The value cannot fall below -5.");
+      let promptInput = prompt("Plese Enter Your Name");
+      setDisplayValue(promptInput);
+      setUserInput("");
     }
   };
 
-  const resetCount = () => {
-    setCount(0);
-    document.querySelector(".count-box ").style.color = "black";
+  const handleReset = () => {
+    setDisplayValue("");
   };
 
   return (
-    <div className="m-1 h-screen flex justify-center items-center  ">
-      <div className=" p-2 h-1/2 min-w-1/4 border-2  border-black rounded-2xl flex flex-col  items-center bg-blue-300 relative">
-        <div className="count-box   text-center p-12 text-5xl"> {count}</div>
-        <h1 className="text-5xl absolute bottom-6 p-3 text-white opacity-15 font-semibold">
-          Simple Counter App
-        </h1>
-        <div className=" flex justify-around gap-5">
-          <button
-            onClick={increaseCount}
-            className="inc h-12 w-24 border-2 rounded-xl border-green-600 p-2  bg-green-300/50 hover:bg-green-400/50 ">
-            {" "}
-            Increase
-          </button>
-          <button
-            onClick={decreaseCount}
-            className="dic h-12 w-24 border-2 rounded-xl  border-red-600 p-2  bg-red-300/50 hover:bg-red-400/50 order-last">
-            Decrease
-          </button>
-          <button
-            onClick={resetCount}
-            className="reset h-12 w-24 border-2 rounded-xl border-black p-2  ">
-            Reset
-          </button>
+    <div className="h-screen flex justify-center items-center">
+      <div className=" m-2 p-5 flex flex-col h-1/2 min-w-1/2  border-2 border-black justify-center gap-8 items-center bg-yellow-200">
+        <input
+          type="text"
+          name="Your Name"
+          id="name"
+          placeholder="Enter Your Text"
+          value={userInput}
+          onChange={handleEnter}
+          className="border-2 border-black  h-12 "
+        />
+        <button
+          onClick={handleClick}
+          className="p-2 h-12 w-24 border-2 border-black rounded-xl bg-green-200 hover:bg-green-400">
+          Show{" "}
+        </button>
+        <div className=" h-12 w-full p-2 border-2 border-black bg-white ">
+          {displayValue}
         </div>
+        <button
+          onClick={handleReset}
+          className="p-2 h-12 w-24 border-2 border-black rounded-xl bg-red-300 hover:bg-red-400">
+          Reset
+        </button>
       </div>
     </div>
   );
